0916f5450773a9f52dc9cf20280a47d9cc0d8bbd
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2008-10-08 22:58:03 +0200

    A8 - RTE

--- A8/A8.cpp
+++ A8/A8.cpp
@@ -1,25 +1,55 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
-template<class a>
-a pow(a x,unsigned int n)
+unsigned int matrix[80];
+unsigned int k,m;
+unsigned int *tab;
+
+inline void iloczyn()
 {
-	a m=x,w=1;
-	while(n)
-	{
-		if(n&1)
-			w*=m;
-		m*=m;
-		n>>=1;
-	}
-	return w;
+    unsigned int *tmp=new unsigned int[k];
+    tmp[0]=0;
+    for(unsigned int i=0;i<k;++i)
+        tmp[0]+=matrix[i]*tab[i];
+    for(unsigned int i=1;i<k;++i)
+        tmp[i]=tab[i-1];
+
+    tmp[0]%=1000;
+    delete [] tab;
+    tab=tmp;
+}
+
+inline void wypisz(unsigned int p)
+{
+    unsigned int tmp=tab[p];
+    printf("%u%u%u\n",tmp/100,(tmp%100)/10,tmp%10);
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	unsigned int i=8;
-	cout<<pow(0,i)<<'\n';
-	return 0;
-}
+    unsigned int z;
+    scanf("%u",&z);
+    tab=new unsigned int[80];
+    while(z--)
+    {
 
+        scanf("%u%u",&k,&m);
+
+        for(unsigned int i=0;i<k;++i)
+            scanf("%u",matrix+i);
+        for(unsigned int i=0;i<k;++i)
+            scanf("%u",tab+i);
+
+        if(k>=m)
+        {
+            wypisz(k-m);
+            continue;
+        }
+
+        for(unsigned int q=0;q<m-k;++q)
+            iloczyn();
+
+        wypisz(0);
+    }
+
+    return 0;
+}
